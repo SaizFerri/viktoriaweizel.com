f193d14f816a28d0e5ba5b528a8d64f982078a33
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-02-24 22:43:10 +0100

    feat: start /blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,20 @@
 import Head from "next/head";
 import React, { FunctionComponent } from "react";
 import Layout from "../../components/Layout";
+import { GetServerSideProps } from "next";
+import { initializeApollo, addApolloState } from "../../lib/apolloClient";
+import GET_ALL_POSTS from "../../graphql/queries/blog/getAllPosts.gql";
+import { useQuery } from "@apollo/client";
+import COLLECTION_STATUS from "../../consts/collectionStatus";
+import BlogCard from "../../components/blog/BlogCard";
+
+const variables = {
+  status: COLLECTION_STATUS,
+};
 
 const BlogPage: FunctionComponent = () => {
+  const { data } = useQuery(GET_ALL_POSTS, { variables });
+
   const head = () => (
     <Head>
       <title>Blog</title>
@@ -11,9 +23,40 @@ const BlogPage: FunctionComponent = () => {
 
   return (
     <Layout head={head}>
-      <div>Blog page</div>
+      <div className="container mt-4">
+        <div className="row">
+          {(data?.items?.posts || []).map(
+            ({ id, title, subtitle, date_created, thumbnail, slug }) => {
+              return (
+                <div className="col-lg-4" key={id}>
+                  <BlogCard
+                    title={title}
+                    createdOn={date_created}
+                    subtitle={subtitle}
+                    thumbnail={thumbnail}
+                    slug={slug}
+                  />
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_ALL_POSTS,
+    variables,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
 };
 
 export default BlogPage;

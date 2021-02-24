931642a3650a8ffba2145f21d94084a21cea886b
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-02-24 19:54:50 +0100

    refactor: masonry

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -1,30 +1,49 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ReactChild } from 'react';
 import ReactMasonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import { SRLWrapper } from 'simple-react-lightbox';
-import Image from './Image';
 
 interface IMasonryProps {
-  items: Record<string, string>[];
+  children: ReactChild | ReactChild[];
+  gutter?: string;
+  columnsCountBreakPoints?: Record<number, number>;
+  lightboxOptions?: Record<string, any>;
+  withLightbox?: boolean;
 }
 
-const lightboxOptions = {
+const defaultColumnsCountBreakPoints = {
+  350: 1,
+  750: 2,
+  900: 3,
+};
+
+const defaultLightboxOptions = {
   buttons: {
     showDownloadButton: false,
   },
 };
 
-const Masonry: FunctionComponent<IMasonryProps> = ({ items }) => {
-  return (
-    <SRLWrapper options={lightboxOptions}>
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-        <ReactMasonry gutter="10px">
-          {items.map((item) => (
-            <Image key={item.id} image={item} alt={item.title} />
-          ))}
-        </ReactMasonry>
-      </ResponsiveMasonry>
-    </SRLWrapper>
+const Masonry: FunctionComponent<IMasonryProps> = ({
+  children,
+  gutter = '0.5rem',
+  columnsCountBreakPoints = defaultColumnsCountBreakPoints,
+  lightboxOptions = defaultLightboxOptions,
+  withLightbox = false,
+}) => {
+  const MansoryContent = () => (
+    <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+      <ReactMasonry gutter={gutter}>{children}</ReactMasonry>
+    </ResponsiveMasonry>
   );
+
+  if (withLightbox) {
+    return (
+      <SRLWrapper options={lightboxOptions}>
+        <MansoryContent />
+      </SRLWrapper>
+    );
+  }
+
+  return <MansoryContent />;
 };
 
 export default Masonry;

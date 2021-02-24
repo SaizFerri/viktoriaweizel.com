18bb12b9ce90a0be005cc86a35ea625bb481b73a
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-02-24 22:42:42 +0100

    feat: add footer content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,27 @@
 import React, { FunctionComponent } from "react";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer: FunctionComponent = () => {
-  return <footer className="footer">Footer</footer>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <small>&#169;&nbsp;Copyright {year} Viktoria Weizel</small>
+        <ul className="footer__social-list">
+          <li className="footer__social-list-item">
+            <a
+              href="https://www.instagram.com/viktoria_weizel/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

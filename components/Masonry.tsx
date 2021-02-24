cd093196c95b93cd09253832376774f83092e20b
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-02-24 14:55:24 +0100

    fix: masonry responsive

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -15,15 +15,15 @@ const lightboxOptions = {
 
 const Masonry: FunctionComponent<IMasonryProps> = ({ items }) => {
   return (
-    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-      <SRLWrapper options={lightboxOptions}>
+    <SRLWrapper options={lightboxOptions}>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <ReactMasonry gutter="10px">
           {items.map((item) => (
             <Image key={item.id} image={item} alt={item.title} />
           ))}
         </ReactMasonry>
-      </SRLWrapper>
-    </ResponsiveMasonry>
+      </ResponsiveMasonry>
+    </SRLWrapper>
   );
 };
 

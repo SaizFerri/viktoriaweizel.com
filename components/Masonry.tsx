e0b61c24c469c28ae7e419ebd44b2aefa537e0b2
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-02-24 22:41:03 +0100

    fix: lightbox

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent, ReactChild } from 'react';
-import ReactMasonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-import { SRLWrapper } from 'simple-react-lightbox';
+import React, { FunctionComponent, ReactChild } from "react";
+import ReactMasonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import SimpleReactLightbox from "simple-react-lightbox";
+import { SRLWrapper } from "simple-react-lightbox";
 
 interface IMasonryProps {
   children: ReactChild | ReactChild[];
+  classNames?: string;
   gutter?: string;
   columnsCountBreakPoints?: Record<number, number>;
   lightboxOptions?: Record<string, any>;
@@ -24,22 +26,27 @@ const defaultLightboxOptions = {
 
 const Masonry: FunctionComponent<IMasonryProps> = ({
   children,
-  gutter = '0.5rem',
+  classNames = "",
+  gutter = "0.5rem",
   columnsCountBreakPoints = defaultColumnsCountBreakPoints,
   lightboxOptions = defaultLightboxOptions,
   withLightbox = false,
 }) => {
   const MansoryContent = () => (
-    <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
-      <ReactMasonry gutter={gutter}>{children}</ReactMasonry>
-    </ResponsiveMasonry>
+    <div className={classNames}>
+      <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+        <ReactMasonry gutter={gutter}>{children}</ReactMasonry>
+      </ResponsiveMasonry>
+    </div>
   );
 
   if (withLightbox) {
     return (
-      <SRLWrapper options={lightboxOptions}>
-        <MansoryContent />
-      </SRLWrapper>
+      <SimpleReactLightbox>
+        <SRLWrapper options={lightboxOptions}>
+          <MansoryContent />
+        </SRLWrapper>
+      </SimpleReactLightbox>
     );
   }
 

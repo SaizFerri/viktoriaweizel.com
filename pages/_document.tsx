1ad6d7563a0bf69e992308053f56808eee5d47bb
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-03-14 17:54:19 +0100

    feat: add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,34 @@ class CustomDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="android-touch-icon"
+            sizes="180x180"
+            href="/favicon/android-chrome-192x192.png"
+          />
+          <link
+            rel="android-touch-icon"
+            sizes="180x180"
+            href="/favicon/android-chrome-512x512.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Open+Sans:wght@300;400;700&display=swap"

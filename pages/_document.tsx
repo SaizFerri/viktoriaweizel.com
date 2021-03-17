39e310ed16ebda70d8be7b935b50cf2edcd86947
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-03-17 21:40:22 +0100

    feat: move gtm scripts to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import APP_URL from "../consts/appUrl";
+import GA_TRACKING_ID from "../consts/gaId";
 
 class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -52,6 +53,26 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Open+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+          {process.env.NODE_ENV === "production" && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />

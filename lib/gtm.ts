75d9a29d5f2ea11979f3a2f63b1962d905a59ac5
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-03-18 22:33:09 +0100

    fix: gtm triggering in develop

--- lib/gtm.ts
+++ lib/gtm.ts
@@ -1,7 +1,9 @@
 import isClient from "../utils/isClient";
 
 export const pageview = (url) => {
-  if (isClient()) {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (isClient() && isProduction) {
     (window as any).dataLayer.push({
       event: "pageview",
       page: url,

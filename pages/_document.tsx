3456298f02e991bc30dacfeb05e81dcd05e69579
Adrian Saiz <adrian.saiz.ferri@outlook.com>
2021-04-24 21:14:53 +0200

    feat: add home gallery slider

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,6 +53,17 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&family=Open+Sans:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charset="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
           {process.env.NODE_ENV === "production" && (
             <script
               dangerouslySetInnerHTML={{
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { GetStaticProps } from "next";
 import Link from "next/link";
+import Slider from "react-slick";
+import { initializeApollo, addApolloState } from "../lib/apolloClient";
+import GET_ALL_POSTS from "../graphql/queries/blog/getAllPosts.gql";
+import GET_ALL_GALLERIES from "../graphql/queries/gallery/getAllGalleries.gql";
+// import { useQuery } from "@apollo/client";
+import COLLECTION_STATUS from "../consts/collectionStatus";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import BlogCard from "../components/blog/BlogCard";
+import { useTheme } from "../components/ThemeProvider";
+import ETheme from "../enums/theme.enum";
+import Image from "../components/Image";
 
-const IndexPage = () => {
+const blogVariables = {
+  status: COLLECTION_STATUS,
+  limit: 4,
+};
+
+const galleryVariables = {
+  status: COLLECTION_STATUS,
+  limit: 4,
+};
+
+const IndexPage = ({ postsData, galleryData }) => {
+  const { data: posts } = postsData;
+  const { data: galleries } = galleryData;
+  const { setTheme } = useTheme();
+
   const seoItem = {
     title: "Viktoria Weizel – Travel, Photography & Lifestyle",
     description: `
@@ -12,28 +39,116 @@ const IndexPage = () => {
     `,
   };
 
+  console.log(galleries);
+
+  var sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    centerPadding: 0,
+    className: "gallery-teaser-slider",
+  };
+
+  useEffect(() => {
+    setTheme(ETheme.DARK);
+  }, []);
+
   return (
     <Layout>
       <SEO title={seoItem.title} item={seoItem} type="website" url="/" />
-      <div className="container">
-        <h1>Viktoria Weizel</h1>
-        <p>
-          Check out my&nbsp;
-          <Link href="/gallery">
-            <a>gallery&nbsp;</a>
-          </Link>
-          and my&nbsp;
-          <Link href="/blog">
-            <a>blog</a>
-          </Link>
-        </p>
-      </div>
+      <Hero />
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <h2 className="section__title">Latest blog posts</h2>
+          </div>
+          <div className="row">
+            {(posts?.items?.posts || []).map(
+              ({ id, title, subtitle, date_created, thumbnail, slug }) => {
+                return (
+                  <div className="col-12 col-md-12 col-lg-6 col-xl-6" key={id}>
+                    <BlogCard
+                      title={title}
+                      createdOn={date_created}
+                      subtitle={subtitle}
+                      thumbnail={thumbnail}
+                      slug={slug}
+                    />
+                  </div>
+                );
+              }
+            )}
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Link href="/blog">
+                <a>See all posts -></a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <h2 className="section__title">Galleries</h2>
+          </div>
+        </div>
+        <Slider {...sliderSettings}>
+          {(galleries?.items?.gallery || []).map(({ name, thumbnail, slug }) => (
+            <div key={slug} className="gallery-teaser-slider__item">
+              <div className="aspect-ratio-16x9 aspect-ratio--cover">
+                <Image image={thumbnail} alt={thumbnail.title} />
+                <h3 className="gallery-teaser-slider__item-title">{name}</h3>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </section>
+      {/* <section className="section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-lg-12">
+              <iframe
+                src="https://open.spotify.com/embed/track/01D7nX5t321cgGgehXH0MU"
+                width="100%"
+                height="80"
+                frameBorder="0"
+                allowTransparency={true}
+                allow="encrypted-media"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </section> */}
     </Layout>
   );
 };
 
-export async function getStaticProps() {
-  return { props: {} };
-}
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  const postsData = await apolloClient.query({
+    query: GET_ALL_POSTS,
+    variables: blogVariables,
+  });
+
+  const galleryData = await apolloClient.query({
+    query: GET_ALL_GALLERIES,
+    variables: galleryVariables,
+  });
+
+  return addApolloState(apolloClient, {
+    props: {
+      postsData,
+      galleryData,
+    },
+    revalidate: 1,
+  });
+};
 
 export default IndexPage;
